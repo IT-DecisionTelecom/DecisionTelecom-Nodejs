39626d0f338b1ba84a2f8cb2f1e67c70ddfd7d7b
Pavlo Goncharenko <pashaghnp@gmail.com>
2022-01-09 21:30:47 +0000

    Add Viber examples

---
+++ examples/viber.js
@@ -0,0 +1,78 @@
+const { ViberMessageType, ViberMessageSourceType, ViberMessage, ViberError, ViberClient } = require('../lib/viber.js');
+
+async function sendTransactionalMessage() {
+    var message = new ViberMessage();
+    message.sender = '380504444444';
+    message.receiver = '380504444444';
+    message.text = 'Viber message text';
+    message.messageType = ViberMessageType.textOnly;
+    message.sourceType = ViberMessageSourceType.Transactional;
+    message.validityPeriod = 3600;
+
+    try {
+        var viberClient = new ViberClient('<YOUR_ACCESS_KEY>');
+        var messageId = await viberClient.sendMessage(message);
+        console.log('Viber message Id: ' + messageId);
+    } catch (error) {
+        if (error instanceof ViberError) {
+            console.log(`Error while sending Viber message.
+Error name: ${error.name}
+Error message: ${error.message}
+Error code: ${error.code}
+Error status: ${error.status}`);
+        } else {
+            console.log(error);
+        }
+    }
+}
+
+async function sendPromotionalMessage() {
+    var message = new ViberMessage();
+    message.sender = '380504444444';
+    message.receiver = '380504444444';
+    message.text = 'Viber message text';
+    message.messageType = ViberMessageType.textImageButton;
+    message.sourceType = ViberMessageSourceType.Promotional;
+    message.imageUrl = 'https://yourdomain.com/images/image.jpg';
+    message.buttonCaption = 'Join Us';
+    message.buttonAction = 'https://yourdomain.com/join-us';
+    message.validityPeriod = 3600;
+
+    try {
+        var viberClient = new ViberClient('<YOUR_ACCESS_KEY>');
+        var messageId = await viberClient.sendMessage(message);
+        console.log('Viber message Id: ' + messageId);
+    } catch (error) {
+        if (error instanceof ViberError) {
+            console.log(`Error while sending Viber message.
+Error name: ${error.name}
+Error message: ${error.message}
+Error code: ${error.code}
+Error status: ${error.status}`);
+        } else {
+            console.log(error);
+        }
+    }
+}
+
+async function getMessageStatus() {
+    try {
+        var viberClient = new ViberClient('<YOUR_ACCESS_KEY>');
+        var receipt = await viberClient.getMessageStatus(380752);
+        console.log(`Viber message status: ${receipt.status.status} (${receipt.status})`);
+    } catch (error) {
+        if (error instanceof ViberError) {
+            console.log(`Error while sending Viber message.
+Error name: ${error.name}
+Error message: ${error.message}
+Error code: ${error.code}
+Error status: ${error.status}`);
+        } else {
+            console.log(error);
+        }
+    }
+}
+
+sendTransactionalMessage();
+sendPromotionalMessage();
+getMessageStatus();
