3517d6e4274725ba6bc73ed1b3171239695d198b
Pavlo Goncharenko <pashaghnp@gmail.com>
2022-01-22 19:20:26 +0100

    Remove sending promotional Viber plus SMS message example

--- examples/viberPlusSms.js
+++ examples/viberPlusSms.js
@@ -35,44 +35,6 @@ async function sendTransactionalMessage() {
     }
 }
 
-async function sendPromotionalMessage() {
-    try {
-        // Create new instance of the viber plus SMS client.
-        var viberPlusSmsClient = new ViberPlusSmsClient('<YOUR_ACCESS_KEY>');
-
-        // Create viber plus SMS message object. This one will be promotional message with message text, image and button.
-        var message = new ViberPlusSmsMessage();
-        message.sender = '380504444444';
-        message.receiver = '380504444444';
-        message.text = 'Viber message text';
-        message.messageType = ViberMessageType.textImageButton;
-        message.sourceType = ViberMessageSourceType.Promotional;
-        message.imageUrl = 'https://yourdomain.com/images/image.jpg';
-        message.buttonCaption = 'Join Us';
-        message.buttonAction = 'https://yourdomain.com/join-us';
-        message.validityPeriod = 3600;
-        message.smsText = "SMS Text";
-
-        // Call client SendMessage method to send viber plus SMS message.
-        var messageId = await viberPlusSmsClient.sendMessage(message);
-
-        // SendMessage method should return Id of the sent Viber plus SMS message.
-        console.log('Viber message Id: ' + messageId);
-    } catch (error) {
-        if (error instanceof ViberError) {
-            // ViberError contains specific DecisionTelecom error with details of what went wrong during the operation.
-            console.log('Error while sending Viber message.')
-            console.log(`Error name: ${error.name}`);
-            console.log(`Error message: ${error.message}`);
-            console.log(`Error code: ${error.code}`);
-            console.log(`Error status: ${error.status}`);
-        } else {
-            // A non-DecisionTelecom error occurred during the operation (like connection error).
-            console.log(error);
-        }
-    }
-}
-
 async function getMessageStatus() {
     try {
         // Create new instance of the viber plus SMS client.
@@ -102,5 +64,4 @@ async function getMessageStatus() {
 }
 
 sendTransactionalMessage();
-sendPromotionalMessage();
 getMessageStatus();
